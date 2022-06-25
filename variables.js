48f7e4c5b6f813230bc68be5051461e7c3231ab4
J-BS-PNG <josuebarrientossandoval@gmail.com>
2022-06-24 18:37:33 -0600

    Variables y funciones
    -Se crearon pagina para ver los ejemplos de curso

---
+++ variables.js
@@ -0,0 +1,25 @@
+// Formas alternas
+// let myVariable = 123
+let myVariable;
+myVariable = 123;
+myVariable = 321
+console.log("Variable let = " + myVariable);
+const PI = 3;
+console.log("constante conts = " + PI);
+
+// PI = 4 esto no se puede
+
+const myObjeto = { a: 3 };
+
+// myObjeto = { b:5 } esto no se puede
+
+myObjeto.a = 5;
+
+let palabra = "Hola";
+let palabra2 = "Mundo"
+
+console.log(`${palabra} ${palabra2}`)
+
+let myBoolTrue = true;
+let myBoolFalse = false;
+
